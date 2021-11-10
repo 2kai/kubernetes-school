0f4fb00f4831bbde046055ff34e090a58d309ef4
Sasha Klepikov <kai@list.ru>
2021-11-10 20:36:42 +0200

    Add Exercise 1.12 (Daily image)

--- README.md
+++ README.md
@@ -17,6 +17,6 @@ kubectl cluster-info
 ##### Part 1
 
 1.01, 1.03 - hash-generator  
-1.02, 1.04, 1.05, 1.06, 1.08 - todo-project  
+1.02, 1.04, 1.05, 1.06, 1.08, 1.12 - todo-project  
 1.07, 1.10, 1.11 - log-output  
 1.09 - ping-pong  
---
+++ todo-project/src/server.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs';
+import fetch from 'node-fetch';
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const dailyImageFilename = '/opt/src/daily.jpg';
+
+app.get('/', function (req, res) {
+    res.json('Kiitti');
+});
+
+app.get('/daily', async function (req, res) {
+    let isFreshFileMissing = false;
+
+    try {
+        fs.accessSync(dailyImageFilename);
+
+        let stats = fs.statSync(dailyImageFilename);
+
+        if (stats.mtime.toDateString() === (new Date()).toDateString()) {
+            console.log('File mtime is today - no need to download file again');
+        } else {
+            isFreshFileMissing = true;
+        }
+    } catch (e) {
+        isFreshFileMissing = true;
+    }
+
+    if (isFreshFileMissing) {
+        console.log('Downloading file');
+        let res = await fetch('https://picsum.photos/1200');
+        let imageResponse = await fetch(res.url);
+        let arrayBuffer = await imageResponse.arrayBuffer();
+        fs.writeFileSync(dailyImageFilename, Buffer.from(arrayBuffer));
+    }
+
+    res.sendFile(dailyImageFilename);
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
